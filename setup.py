8bab3150df64b0cd858ae78a91d0a94e4de4808e
Daniel Robbins <drobbins@funtoo.org>
2012-05-07 16:43:07 +0000

    GPLv2 licensing fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = 'ZenPacks.sodonnell.TransformReport'
 VERSION = '1.0'
 AUTHOR = 'Steven O\'Donnell'
-LICENSE = ''
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.sodonnell']
 PACKAGES = ['ZenPacks', 'ZenPacks.sodonnell', 'ZenPacks.sodonnell.TransformReport']
 INSTALL_REQUIRES = []
